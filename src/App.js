f552e6cf485b57b9b93484e324e596296e351a0e
unknown <siamur01@gmail.com>
2021-09-19 14:31:50 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
+        <h3>Yo Yo REACT mama</h3>
+        <p>happy happy react</p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>siamur</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
